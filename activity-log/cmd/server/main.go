839ad6412ad425a847d4b4da40fa3a6f71a66b1a
Adam Gordon Bell <adam@corecursive.com>
2022-02-10 16:20:07 -0500

    feedback

--- activity-log/cmd/server/main.go
+++ activity-log/cmd/server/main.go
@@ -5,7 +5,7 @@ import (
 	"net"
 
 	"github.com/adamgordonbell/cloudservices/activity-log/internal/server"
-	"google.golang.org/grpc/reflection"
+	// "google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -19,7 +19,7 @@ func main() {
 	log.Printf("Listening on %s", port)
 	srv := server.NewGRPCServer()
 	// Register reflection service on gRPC server.
-	reflection.Register(srv)
+	// reflection.Register(srv)
 	if err := srv.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}

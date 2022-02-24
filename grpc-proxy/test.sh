af98d343d9b2015f6f616031d14d04957634aeac
Adam Gordon Bell <adam@corecursive.com>
2022-02-24 13:57:34 -0500

    WIP

--- grpc-proxy/test.sh
+++ grpc-proxy/test.sh
@@ -1,7 +1,20 @@
+#!/usr/bin/env sh
+set -e
+# set -x
+
 echo "start up"
 ./grpc-proxy &
 
-
-echo "Testing"
+echo "=== Insert Test Data ==="
 curl  -X POST -s localhost:8081/api.v1.Activity_Log/Insert -d \
 '{"activity": {"description": "christmas eve bike class", "time":"2021-12-09T16:34:04Z"}}'
+
+echo "=== Test Retrieve Descriptions ==="
+curl  -X POST -s localhost:8081/api.v1.Activity_Log/Retrieve -d \
+'{ "id": 1 }' | grep -q 'christmas eve bike class'
+
+echo "=== Test List ==="
+curl  -X POST -s localhost:8081/api.v1.Activity_Log/List -d \
+'{ "offset": 0 }' 
+
+echo "Success"

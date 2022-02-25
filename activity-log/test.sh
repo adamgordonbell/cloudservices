569f3f55f039718fd1cf1a5e8b7a20be401fb298
Adam Gordon Bell <adam@corecursive.com>
2022-02-25 15:25:01 -0500

    WIP

--- activity-log/test.sh
+++ activity-log/test.sh
@@ -2,13 +2,13 @@
 set -e
 # set -x
 
-echo "=== Test Reflection API ==="
+echo "=== GRPC: Test Reflection API ==="
 grpcurl -insecure localhost:8080 describe
 
-echo "=== Test Cert Authority ==="
+echo "=== GRPC: Test Cert Authority ==="
 grpcurl -cacert=./certs/ca.pem localhost:8080 describe
 
-echo "=== Insert Test Data ==="
+echo "=== GRPC: Insert Test Data ==="
 
 grpcurl -insecure -d  '{ "description": "christmas eve bike class" }' localhost:8080 api.v1.Activity_Log/Insert
 
@@ -16,7 +16,7 @@ grpcurl -insecure -d  '{ "description": "cross country skiing is horrible and co
 
 grpcurl -insecure -d  '{ "description": "sledding with nephew" }' localhost:8080 api.v1.Activity_Log/Insert
 
-echo "=== Test Retrieve Descriptions ==="
+echo "=== GRPC: Test Retrieve Descriptions ==="
 
 grpcurl -insecure -d '{ "id": 1 }' localhost:8080 api.v1.Activity_Log/Retrieve
 
@@ -24,23 +24,23 @@ grpcurl -insecure -d '{ "id": 1 }' localhost:8080 api.v1.Activity_Log/Retrieve |
 grpcurl -insecure -d '{ "id": 2 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'cross country skiing'
 grpcurl -insecure -d '{ "id": 3 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'sledding'
 
-echo "=== Test List ==="
+echo "=== GRPC: Test List ==="
 
 grpcurl -insecure localhost:8080 api.v1.Activity_Log/List | jq '.activities | length' |  grep -q '3'
 grpcurl -insecure -d '{ "offset": 3 }' localhost:8080 api.v1.Activity_Log/List | jq '.activities | length'|  grep -q '0'
 
 echo "=== Test Rest API ==="
 
-echo "=== Insert Test Data ==="
-curl -X POST -s localhost:8080/api.v1.Activity_Log/Insert -d \
+echo "=== REST: Insert Test Data ==="
+curl -k -X POST -s https://localhost:8080/api.v1.Activity_Log/Insert -d \
 '{"activity": {"description": "christmas eve bike class", "time":"2021-12-09T16:34:04Z"}}'
 
-echo "=== Test Retrieve Descriptions ==="
-curl -X POST -s localhost:8080/api.v1.Activity_Log/Retrieve -d \
+echo "=== REST: Test Retrieve Descriptions ==="
+curl -k -X POST -s https://localhost:8080/api.v1.Activity_Log/Retrieve -d \
 '{ "id": 1 }' | grep -q 'christmas eve bike class'
 
-echo "=== Test List ==="
-curl -X POST -s localhost:8080/api.v1.Activity_Log/List -d \
+echo "=== REST: Test List ==="
+curl -k -X POST -s https://localhost:8080/api.v1.Activity_Log/List -d \
 '{ "offset": 0 }' 
 
 echo "Success"
--- grpc-proxy/test.sh
+++ grpc-proxy/test.sh
@@ -6,15 +6,15 @@ echo "start up"
 ./grpc-proxy &
 
 echo "=== Insert Test Data ==="
-curl  -X POST -s localhost:8081/api.v1.Activity_Log/Insert -d \
+curl -X POST -s localhost:8081/api.v1.Activity_Log/Insert -d \
 '{"activity": {"description": "christmas eve bike class", "time":"2021-12-09T16:34:04Z"}}'
 
 echo "=== Test Retrieve Descriptions ==="
-curl  -X POST -s localhost:8081/api.v1.Activity_Log/Retrieve -d \
+curl -X POST -s localhost:8081/api.v1.Activity_Log/Retrieve -d \
 '{ "id": 1 }' | grep -q 'christmas eve bike class'
 
 echo "=== Test List ==="
-curl  -X POST -s localhost:8081/api.v1.Activity_Log/List -d \
+curl -X POST -s localhost:8081/api.v1.Activity_Log/List -d \
 '{ "offset": 0 }' 
 
 echo "Success"

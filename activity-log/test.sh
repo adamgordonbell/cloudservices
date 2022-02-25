601e6379d895314801cee4c253fa36a2c2c89224
Adam Gordon Bell <adam@corecursive.com>
2022-02-25 11:51:14 -0500

    WIP

--- activity-log/test.sh
+++ activity-log/test.sh
@@ -3,27 +3,44 @@ set -e
 # set -x
 
 echo "=== Test Reflection API ==="
-# grpcurl -plaintext localhost:8080 describe
+grpcurl -insecure localhost:8080 describe
+
+echo "=== Test Cert Authority ==="
+grpcurl -cacert=./certs/ca.pem localhost:8080 describe
 
 echo "=== Insert Test Data ==="
 
-grpcurl -plaintext -d  '{ "description": "christmas eve bike class" }' localhost:8080 api.v1.Activity_Log/Insert
+grpcurl -insecure -d  '{ "description": "christmas eve bike class" }' localhost:8080 api.v1.Activity_Log/Insert
 
-grpcurl -plaintext -d  '{ "description": "cross country skiing is horrible and cold" }' localhost:8080 api.v1.Activity_Log/Insert
+grpcurl -insecure -d  '{ "description": "cross country skiing is horrible and cold" }' localhost:8080 api.v1.Activity_Log/Insert
 
-grpcurl -plaintext -d  '{ "description": "sledding with nephew" }' localhost:8080 api.v1.Activity_Log/Insert
+grpcurl -insecure -d  '{ "description": "sledding with nephew" }' localhost:8080 api.v1.Activity_Log/Insert
 
 echo "=== Test Retrieve Descriptions ==="
 
-grpcurl -plaintext -d '{ "id": 1 }' localhost:8080 api.v1.Activity_Log/Retrieve
+grpcurl -insecure -d '{ "id": 1 }' localhost:8080 api.v1.Activity_Log/Retrieve
 
-grpcurl -plaintext -d '{ "id": 1 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'christmas eve bike class'
-grpcurl -plaintext -d '{ "id": 2 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'cross country skiing'
-grpcurl -plaintext -d '{ "id": 3 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'sledding'
+grpcurl -insecure -d '{ "id": 1 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'christmas eve bike class'
+grpcurl -insecure -d '{ "id": 2 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'cross country skiing'
+grpcurl -insecure -d '{ "id": 3 }' localhost:8080 api.v1.Activity_Log/Retrieve | grep -q 'sledding'
 
 echo "=== Test List ==="
 
-grpcurl -plaintext localhost:8080 api.v1.Activity_Log/List | jq '.activities | length' |  grep -q '3'
-grpcurl -plaintext -d '{ "offset": 3 }' localhost:8080 api.v1.Activity_Log/List | jq '.activities | length'|  grep -q '0'
+grpcurl -insecure localhost:8080 api.v1.Activity_Log/List | jq '.activities | length' |  grep -q '3'
+grpcurl -insecure -d '{ "offset": 3 }' localhost:8080 api.v1.Activity_Log/List | jq '.activities | length'|  grep -q '0'
+
+echo "=== Test Rest API ==="
+
+echo "=== Insert Test Data ==="
+curl -X POST -s localhost:8080/api.v1.Activity_Log/Insert -d \
+'{"activity": {"description": "christmas eve bike class", "time":"2021-12-09T16:34:04Z"}}'
+
+echo "=== Test Retrieve Descriptions ==="
+curl -X POST -s localhost:8080/api.v1.Activity_Log/Retrieve -d \
+'{ "id": 1 }' | grep -q 'christmas eve bike class'
+
+echo "=== Test List ==="
+curl -X POST -s localhost:8080/api.v1.Activity_Log/List -d \
+'{ "offset": 0 }' 
 
 echo "Success"

092f73aeb1600bf9b924cf19516e9fa0ed070353
Adam Gordon Bell <adam@corecursive.com>
2022-02-28 10:33:16 -0500

    working CA certs

--- Earthfile
+++ Earthfile
@@ -4,4 +4,21 @@ FROM golang:1.17-alpine3.13
 test-all:
     BUILD ./activity-log+test
     BUILD ./activity-client+test
+    BUILD ./grpc-proxy+test
+
+up:
+    LOCALLY
+    WITH DOCKER \
+        --load=./activity-log+docker \
+        --load=./grpc-proxy+docker 
+        RUN docker rm -f  activity-log || false && \
+            docker rm -f  grpc-proxy || false && \
+            docker run -d --name activity-log -p 8080:8080 agbell/cloudservices/activity-log && \
+            docker run -d --name grpc-proxy -p 8081:8081 agbell/cloudservices/grpc-proxy 
+    END
+
+down:
+    LOCALLY
+    RUN docker stop activity-log 
+    RUN docker stop grpc-proxy
 
--- grpc-proxy/main.go
+++ grpc-proxy/main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 
 	api "github.com/adamgordonbell/cloudservices/activity-log/api/v1"
 )
@@ -18,8 +18,12 @@ func main() {
 	log.Println("Starting listening on port 8081")
 	port := ":8081"
 	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err := api.RegisterActivity_LogHandlerFromEndpoint(context.Background(), mux, grpcServerEndpoint, opts)
+	tlsCreds, err := credentials.NewClientTLSFromFile("../activity-log/certs/ca.pem", "")
+	if err != nil {
+		log.Fatalf("No cert found: %v", err)
+	}
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+	err = api.RegisterActivity_LogHandlerFromEndpoint(context.Background(), mux, grpcServerEndpoint, opts)
 	if err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}

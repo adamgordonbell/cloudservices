ccca4f971d8c04ff8d221855f70fb2330aa0b1d4
Adam Gordon Bell <adam@corecursive.com>
2022-04-11 09:59:20 -0400

    WIP

--- lambda/go.mod
+++ lambda/go.mod
@@ -1,3 +1,5 @@
 module github.com/adamgordonbell/cloudservices/lambda
 
 go 1.17
+
+require github.com/aws/aws-lambda-go v1.29.0
--- lambda/main.go
+++ lambda/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Event struct {
+	QueryStringParameters QueryStringParameters `json:"queryStringParameters"`
+}
+type QueryStringParameters struct {
+	url string
+}
+
+type Response struct {
+	StatusCode int               `json:"statusCode"`
+	Body       string            `json:"body"`
+	Headers    map[string]string `json:"headers"`
+}
+
+func HandleLambdaEvent(event Event) (Response, error) {
+	return Response{Body: fmt.Sprintf("got url: %s", event.QueryStringParameters.url), StatusCode: 200}, nil
+}
+
 func main() {
-	println("Hello World")
+	lambda.Start(HandleLambdaEvent)
 }

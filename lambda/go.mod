de7c5ccf3235923f4e03e8b4877c4f24641d7531
Adam Gordon Bell <adam@corecursive.com>
2022-04-12 10:30:15 -0400

    s3 working

--- lambda/go.mod
+++ lambda/go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/aws/aws-lambda-go v1.29.0
+	github.com/aws/aws-sdk-go v1.43.37
 	github.com/go-shiori/go-readability v0.0.0-20220215145315-dd6828d2f09b
 )
 
@@ -11,8 +12,9 @@ require (
 	github.com/andybalholm/cascadia v1.2.0 // indirect
 	github.com/go-shiori/dom v0.0.0-20210627111528-4e4722cd0d65 // indirect
 	github.com/gogs/chardet v0.0.0-20191104214054-4b6791f73a28 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- lambda/main.go
+++ lambda/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,6 +10,9 @@ import (
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	readability "github.com/go-shiori/go-readability"
 )
 
@@ -25,11 +29,17 @@ type Response struct {
 	Headers    map[string]string `json:"headers"`
 }
 
+type App struct {
+	AwsSession *session.Session
+	S3         *s3.S3
+}
+
 var headersTXT = map[string]string{"Content-Type": "application/json"}
 
-func HandleLambdaEvent(event Event) (Response, error) {
+func (app App) HandleLambdaEvent(event Event) (Response, error) {
 
 	if event.QueryStringParameters.Url == "" {
+		log.Println("Home page request")
 		bytes, err := ioutil.ReadFile("index.txt")
 		if err != nil {
 			fmt.Print(err)
@@ -37,23 +47,77 @@ func HandleLambdaEvent(event Event) (Response, error) {
 		}
 		return Response{Body: string(bytes), StatusCode: 200, Headers: headersTXT}, nil
 	} else {
-		article, err := readability.FromURL(event.QueryStringParameters.Url, 30*time.Second)
+		log.Printf("request: %v", event.QueryStringParameters.Url)
+		cache, err := app.get(event.QueryStringParameters.Url)
 		if err != nil {
-			log.Printf("Error: failed to parse (422) %v: %v", event.QueryStringParameters.Url, err)
-			return Response{StatusCode: 422}, err
+			log.Printf("cache miss %v", err)
+			log.Printf("No cache found: %v", event.QueryStringParameters.Url)
+			article, err := readability.FromURL(event.QueryStringParameters.Url, 30*time.Second)
+			if err != nil {
+				log.Printf("Error: failed to parse (422) %v: %v", event.QueryStringParameters.Url, err)
+				return Response{StatusCode: 422}, err
+			}
+			cmd := exec.Command("lynx", "--stdin", "--dump", "--nolist", "--assume_charset=utf8")
+			cmd.Stdin = strings.NewReader(article.Content)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				log.Printf("Error: failed to lynx %v: %v", event.QueryStringParameters.Url, err)
+				return Response{StatusCode: 500}, err
+			}
+			body := article.Title + "\n\n" + string(out) + "\n\n" + "Text-Mode By Earthly.dev"
+			err = app.put(event.QueryStringParameters.Url, body)
+			if err != nil {
+				log.Printf("Error: failed to put %v: %v", event.QueryStringParameters.Url, err)
+			}
+			return Response{Body: body, StatusCode: 200, Headers: headersTXT}, nil
+		} else {
+			log.Printf("cache found: %v", event.QueryStringParameters.Url)
+			return Response{Body: cache, StatusCode: 200, Headers: headersTXT}, nil
 		}
-		cmd := exec.Command("lynx", "--stdin", "--dump", "--nolist", "--assume_charset=utf8")
-		cmd.Stdin = strings.NewReader(article.Content)
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			log.Printf("Error: failed to lynx %v: %v", event.QueryStringParameters.Url, err)
-			return Response{StatusCode: 500}, err
-		}
-		body := article.Title + "\n\n" + string(out) + "\n\n" + "Text-Mode By Earthly.dev"
-		return Response{Body: body, StatusCode: 200, Headers: headersTXT}, nil
 	}
 }
 
+func (app App) put(url string, result string) error {
+	input := &s3.PutObjectInput{
+		Body:   strings.NewReader(result),
+		Bucket: aws.String("text-mode"),
+		Key:    aws.String(url),
+	}
+	r, err := app.S3.PutObject(input)
+	if err != nil {
+		return fmt.Errorf("failed to store result: %w", err)
+	}
+	log.Printf("Stored result: %v", r)
+	return nil
+}
+
+func (app App) get(url string) (string, error) {
+	req := &s3.GetObjectInput{
+		Bucket: aws.String("text-mode"),
+		Key:    aws.String(url),
+	}
+	r, err := app.S3.GetObject(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to get result: %w", err)
+	}
+	defer r.Body.Close()
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(r.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to get S3 result: %w", err)
+	}
+	return buf.String(), nil
+}
+
 func main() {
-	lambda.Start(HandleLambdaEvent)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+	if err != nil {
+		log.Fatalf("failed to create AWS session, %v", err)
+	}
+	s3 := s3.New(sess)
+	app := App{AwsSession: sess, S3: s3}
+
+	lambda.Start(app.HandleLambdaEvent)
 }

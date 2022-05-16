f3a6d70f5c3c15125ae281c93691f4294bd07400
Adam Gordon Bell <adam@corecursive.com>
2022-05-16 16:15:23 -0400

    WIP

--- lambda-api/go.mod
+++ lambda-api/go.mod
@@ -6,4 +6,7 @@ require (
 	github.com/aws/aws-lambda-go v1.31.1
 	github.com/awslabs/aws-lambda-go-api-proxy v0.13.2
 	github.com/gorilla/mux v1.7.4
+	github.com/motemen/go-loghttp v0.0.0-20170804080138-974ac5ceac27
 )
+
+require github.com/motemen/go-nuts v0.0.0-20210915132349-615a782f2c69 // indirect
--- lambda-api/main.go
+++ lambda-api/main.go
@@ -1,21 +1,61 @@
 package main
 
 import (
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
 	"github.com/gorilla/mux"
+	_ "github.com/motemen/go-loghttp/global"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, World!")
+func TextModeHomeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Text Mode page request")
+	bytes, err := ioutil.ReadFile("textmode.txt")
+	if err != nil {
+		log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(bytes)
 }
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Home page request")
+	log.Println(r.RequestURI)
+	bytes, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(bytes)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	log.Println("Starting up")
 	r := mux.NewRouter()
+	r.HandleFunc("/text-mode", TextModeHomeHandler)
+	r.HandleFunc("/lambda-api/text-mode", TextModeHomeHandler)
 	r.HandleFunc("/", HomeHandler)
 	http.Handle("/", r)
 
-	lambda.Start(httpadapter.New(http.DefaultServeMux).ProxyWithContext)
+	r.Use(loggingMiddleware)
+
+	adapter := gorillamux.NewV2(r)
+
+	lambda.Start(adapter.ProxyWithContext)
 }

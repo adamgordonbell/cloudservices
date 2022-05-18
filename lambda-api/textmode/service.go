e14b99b52f66e9e6280eac3fd70aaf78eb3de82a
Adam Gordon Bell <adam@corecursive.com>
2022-05-18 11:11:40 -0400

    stuff

--- lambda-api/textmode/service.go
+++ lambda-api/textmode/service.go
@@ -62,7 +62,7 @@ func (app App) TextModeHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, _ = io.WriteString(w, result)
 }
